d2d062d64e8b0934ea2344b1350cb2d44a33fc9e
ashu565 <ashu9993063472@gmail.com>
2022-03-04 19:02:08 +0530

    update for T

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import VideoJS from './VideoJS'; // point to where the functional component is stored
+import YouTube from 'react-youtube';
 
 const App = () => {
   const playerRef = useRef(null);
@@ -11,7 +12,7 @@ const App = () => {
     fluid: true,
     sources: [
       {
-        src: 'https://d1fyajldp701ol.cloudfront.net/61ec0af05f53bc1805a694071644024199-620658.mp4#t=0.3',
+        src: 'https://d1fyajldp701ol.cloudfront.net/61ec0af05f53bc1805a694071644024199-620658.mp4',
         type: 'video/mp4',
       },
     ],
@@ -30,11 +31,22 @@ const App = () => {
 
   return (
     <>
-      <div>Rest of app here</div>
-
-      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-
-      <div>Rest of app here</div>
+      <YouTube
+  videoId={string}                  // defaults -> null
+  id={string}                       // defaults -> null
+  className={string}                // defaults -> null
+  containerClassName={string}       // defaults -> ''
+  title={string}                    // defaults -> null
+  opts={obj}                        // defaults -> {}
+  onReady={func}                    // defaults -> noop
+  onPlay={func}                     // defaults -> noop
+  onPause={func}                    // defaults -> noop
+  onEnd={func}                      // defaults -> noop
+  onError={func}                    // defaults -> noop
+  onStateChange={func}              // defaults -> noop
+  onPlaybackRateChange={func}       // defaults -> noop
+  onPlaybackQualityChange={func}    // defaults -> noop
+/>
     </>
   );
 };
